f76cb6b3ed281059f8faa8c99600ea2e36ab7edd
Cristian Schmidt <cristian.schmidt@villelabrasillabs.com>
2026-02-26 17:08:28 -0300

    Removing consoles logs from UseCase Artists

--- KpopHall.Application/Artists/UpdateArtists/UpdateArtistUseCase.cs
+++ KpopHall.Application/Artists/UpdateArtists/UpdateArtistUseCase.cs
@@ -18,19 +18,11 @@ public class UpdateArtistUseCase
         if (artist == null)
             throw new DomainException("Artist not found.");
 
-
-        Console.WriteLine($"Nome do artista no banco: {artist.Name}");
-        Console.WriteLine ($"Novo nome do artista: {request.Name}");
-
         artist.Rename(request.Name);
-
 
         await repository.UpdateAsync(artist);
 
-        Console.WriteLine($"Nome do artista após atualização: {artist.Name}");
-
         var updatedArtist = await repository.GetByIdAsync(id);
-        Console.WriteLine($"Nome após o update: {updatedArtist.Name}");
 
         return new UpdateArtistResponse
         {
